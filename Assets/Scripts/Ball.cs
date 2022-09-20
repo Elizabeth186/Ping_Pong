071097ae522919a4c11ea191e7200b1eecf407cd
Elizabeth Suarez <89174310+Elizabeth186@users.noreply.github.com>
2022-09-20 14:14:32 -0600

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+   [SerializeField]  private float initialVelocity = 4f;
+   [SerializeField]  private float velocityMultiplayer = 1.1f;
+   private Rigidbody2D ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody2D>();
+        BallPlay();
+    }
+
+    // Update is called once per frame
+
+     void BallPlay(){
+
+        float x = Random.Range(0,2) == 0 ? 1 : -1;
+        float y = Random.Range(0,2) == 0 ? 1 : -1;
+        ball.velocity = new Vector2(x, y) * initialVelocity;
+}
+
+private void OnTriggerEnter2D(Collider2D collision) {
+
+    if(collision.gameObject.CompareTag("Player1")){
+    GameManager.Instance.Paddle1Score();
+    GameManager.Instance.Restart();
+    BallPlay();
+    }
+    else{
+        GameManager.Instance.Paddle2Score();
+         GameManager.Instance.Restart();
+         BallPlay();
+    }
+}
+    
+}
---
+++ Assets/Scripts/Players.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players : MonoBehaviour
+{
+    public float velocidad = 0.5f;
+     public string playerName = "Jugador1"; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         float vertitalplayer = Input.GetAxis(playerName) * velocidad;
+
+        Vector2 player = new Vector2(0f, vertitalplayer);
+        
+        transform.Translate(player);
+
+         if(transform.position.y >2.30f){
+            Vector2 positionnew = new Vector2(transform.position.x,2.30f);
+            transform.position = positionnew;
+        }else if(transform.position.y < -2.30f){
+             Vector2 positionnew = new Vector2(transform.position.x, -2.30f);
+            transform.position = positionnew;
+        }
+
+    }
+
+    }
+
